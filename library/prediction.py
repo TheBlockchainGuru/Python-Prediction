46eae7488480ee18ac5d00713e7d007dd8c51e6f
Hot-Gambler <dmmeddin077@gmail.com>
2022-02-16 11:17:20 -0800

    ...

--- library/prediction.py
+++ library/prediction.py
@@ -20,7 +20,7 @@ class Token:
         session.mount('https://', adapter)
         self.i = 0
         self.j = 0
-        self.web3 = Web3(Web3.HTTPProvider(self.provider, session=session))
+        self.web3 = Web3(Web3.WebsocketProvider(self.provider, websocket_timeout=60))
         self.wallet_address = None
         self.signed_tx = None
         self.signed_tx_bear = None
